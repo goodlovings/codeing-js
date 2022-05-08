6e32278c80f02f0f9f8a8fcb66e7eb5f0ae4cfc1
goodloving <953370874@qq.com>
2022-05-08 10:31:02 +0800

    flat add annotation

--- coding/flatArray.js
+++ coding/flatArray.js
@@ -14,27 +14,30 @@ function myFlat1(arr) {
 
 /**
  * 使用递归方式打平数组
- * @param {*} arr 
+ * @param {*} arr
  */
 function myFlatRecursion(arr) {
     if (arr.length === 0) {
         return;
     };
     return arr.reduce((pre, cur) => {
+        // 如果当前元素是数组，调用递归函数本身，如果不是数组，组合在一起
         return Array.isArray(cur) ? [...pre, ...myFlatRecursion(cur)] : [...pre, cur]
-    }, []);
+    }, []); // 默认第一个元素为空数组
 }
 
 /**
  * 使用迭代方式打平数组
- * @param {*} arr 
- * @returns 
+ * @param {*} arr
+ * @returns
  */
 function myFlatIterate(arr){
     if(!arr.length){
         return;
     }
+    // 当arr数组中存在数组元素，一遍一遍判断和打平
     while(arr.some(item => Array.isArray(item))){
+        // 打平当前层，返回打平一层后的数组arr
         arr = [].concat(...arr);
     }
     return arr;
